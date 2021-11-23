9f0d3d68a941e7d44c60c8cd7a997937a52a565f
hengjiwang <hengjw@uw.edu>
2021-11-22 16:14:47 -0800

    update the pressure driven model formulations, and add an equivalent single cell simulation script

--- hydranerv/models/bursting/pressure/pressure_controller.py
+++ hydranerv/models/bursting/pressure/pressure_controller.py
@@ -11,12 +11,13 @@ class PressureController:
         self.k_e = .04
         self.alpha = 1
         self.beta = .2
-        self.tau_c = 5
+        self.tau_p = 5
         self.reset()
 
     def reset(self):
         self.p_train = [0]
         self.vsize_train = [0]
+        self.p_ext_train = [0]
 
     def t(self):
         return self.neuron.t
@@ -24,44 +25,53 @@ class PressureController:
     def vsize(self):
         return self.vsize_train[-1]
 
+    def p_ext(self):
+        return self.p_ext_train[-1]
+
     def pressure(self):
         return self.p_train[-1]
 
     def update_vsize(self):
         """update spikes effect"""
-        vsize = self.k_in * self.t()
-        for t_spike in self.neuron.spike_train:
-            vsize -= self.k_e
-        self.vsize_train.append(vsize)
+        if self.neuron.spike_train and self.neuron.spike_train[-1] == self.t() - self.dt:
+            self.vsize_train.append(self.vsize() - self.k_e)
+        else:
+            self.vsize_train.append(self.vsize() + self.dt * self.k_in)
+
+    def update_p_ext(self):
+        """update external pressure"""
+        if self.neuron.spike_train and self.neuron.spike_train[-1] == self.t() - self.dt:
+            self.p_ext_train.append(self.p_ext() + self.beta)
+        else:
+            self.p_ext_train.append(self.p_ext() - self.dt * self.p_ext() / self.tau_p)
 
     def update_p(self):
         """update pressure"""
-        p = self.alpha * self.vsize()
-        for t_spike in self.neuron.spike_train:
-            p += self.beta * np.exp(- (self.t() - t_spike) / self.tau_c)
+        p = self.alpha * self.vsize() + self.p_ext()
         self.p_train.append(p)
 
     def update(self):
         self.update_vsize()
+        self.update_p_ext()
         self.update_p()
 
     def disp(self, xmin=None, xmax=None):
         time_axis = np.arange(self.dt, len(self.p_train) * self.dt, self.dt)
-        fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 8))
-        ax1.plot(time_axis, self.neuron.v_train[1:])
-        ax1.set_ylabel('Potential (mV)')
-        ax2.plot(time_axis, self.vsize_train[1:])
-        ax2.set_ylabel('Vacuole size')
-        ax3.plot(time_axis, self.p_train[1:])
-        ax3.set_ylabel('Pressure (Pa)')
-        ax4.plot(time_axis, self.neuron.i_mem_train[1:])
-        ax4.set_ylabel('I_mem (nA)')
-        ax4.set_xlabel('Time (s)')
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 7))
+        ax1.plot(time_axis, self.neuron.v_train[1:], 'b')
+        ax1.set_ylabel('potential (mV)')
+        ax2.plot(time_axis, self.vsize_train[1:], 'g', label='vacuole size')
+        ax2.plot(time_axis, self.p_ext_train[1:], 'r', label='external')
+        ax2.plot(time_axis, self.p_train[1:], 'k', label='pressure')
+        ax2.legend()
+        ax2.set_ylabel('driving terms')
+        ax3.plot(time_axis, self.neuron.i_mem_train[1:], 'gray')
+        ax3.set_ylabel('I_mem (nA)')
+        ax3.set_xlabel('Time (s)')
         if xmin and xmax:
             ax1.set_xlim(xmin, xmax)
             ax2.set_xlim(xmin, xmax)
             ax3.set_xlim(xmin, xmax)
-            ax4.set_xlim(xmin, xmax)
         plt.show()
 
 
