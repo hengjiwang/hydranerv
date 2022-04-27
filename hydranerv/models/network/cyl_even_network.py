6618a35ae5998956087073299dae1435ea9e93f4
hengjiwang <hengjw@uw.edu>
2022-04-27 15:34:51 -0700

    add cyl even network

---
+++ hydranerv/models/network/cyl_even_network.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from hydranerv.models.network.neuron import Neuron
+from hydranerv.models.network.network import Network
+from hydranerv.utils import utils
+
+class CylEvenNetwork(Network):
+    """a model of cylindrical neuronal networks where neurons are evenly distributed"""
+    def __init__(self, num_lon=15, num_cir=15, gc=1, dt=.01, tmax=1000, pacemakers=[],
+                t_ref=.1, conn_type='gap_junction', t_syn=.01, wnoise=0, is_semi_pm=False,
+                prob_lon=1, prob_cir=1, rho=.5, seed=0):
+        """constructor"""
+        self.locations = [] # 2d matrix (phi, z)
+        self.num_lon = num_lon
+        self.num_cir = num_cir
+        self.prob_lon = prob_lon
+        self.prob_cir = prob_cir
+        self.rho = rho
+        super().__init__(num_lon * num_cir, [], gc, dt, tmax, pacemakers, t_ref, conn_type,
+                        t_syn, wnoise, is_semi_pm, seed)
+
+    def set_locations(self):
+        """set locations for neurons"""
+        dphi = 2 * np.pi / self.num_cir
+        dz = 1 / self.num_lon
+        for i, z in enumerate(np.arange(1-dz, -dz, -dz)):
+            for j, phi in enumerate(np.arange(0, 2 * np.pi, dphi)):
+                self.locations.append((phi, z))
+
+    def set_connections(self):
+        """set connections for neurons"""
+        self.set_locations()
+        self.neighbors = [[] for _ in range(self.num)]
+        for i in range(self.num_lon):
+            for j in range(self.num_cir):
+                curr = i * self.num_cir + j
+
+                # set longitudinal edge
+                if i < self.num_lon - 1:
+                    below = (i + 1) * self.num_cir + j
+                    if np.random.rand() < self.prob_lon:
+                        self.add_edge((curr, below))
+
+                # set circular edge
+                nex = i * self.num_cir + j + 1
+                if j == self.num_cir - 1:
+                    nex = i * self.num_cir
+                if np.random.rand() < self.prob_cir or i <= 1 or i == self.num_lon - 1:
+                    self.add_edge((curr, nex))
+
+
+
+    def disp_network(self, figsize=(8,8), show_pm=True):
+        """display the network connectivity"""
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_box_aspect((2*self.rho, 2*self.rho, 1))
+
+        # Plot the surface
+        u = np.linspace(0, 2 * np.pi, 50)
+        h = np.linspace(0, 1, 20)
+        x = np.outer(self.rho * np.sin(u), np.ones(len(h)))
+        y = np.outer(self.rho * np.cos(u), np.ones(len(h)))
+        z = np.outer(np.ones(len(u)), h)
+        ax.plot_surface(x, y, z, color='lightgreen', alpha=.2)
+
+        # Plot neurons
+        for i, loc in enumerate(self.locations):
+            phi = loc[0]
+            z = loc[1]
+            if show_pm and i in self.pacemakers:
+                ax.scatter(self.rho * np.cos(phi),
+                        self.rho * np.sin(phi),
+                        z,
+                        color='#1f77b4', alpha=1, edgecolors=['r'])
+            else:
+                ax.scatter(self.rho * np.cos(phi),
+                        self.rho * np.sin(phi),
+                        z,
+                        color='#1f77b4', alpha=1)
+
+        # plot edges
+        for edge in self.edges:
+            phi1 = self.locations[edge[0]][0]
+            z1 = self.locations[edge[0]][1]
+            phi2 = self.locations[edge[1]][0]
+            z2 = self.locations[edge[1]][1]
+            ax.plot([self.rho * np.cos(phi1), self.rho * np.cos(phi2)],
+                    [self.rho * np.sin(phi1), self.rho * np.sin(phi2)],
+                    [z1, z2],
+                    color='k', lw=.5)
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+
+        plt.show()
